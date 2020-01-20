720d1dfc70d3757ebe29b27add52f3fb598a01c4
Timo Kock <timo.kock@movicoders.com>
2020-01-20 15:49:50 +0100

    Delete Profile Experience
    Added feature Delete experience from profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -198,5 +198,30 @@ router.put('/experience', [auth,
     }
 );
 
+//@route    DELETE api/profile/experience/:exp_id
+//@desc     Delete experience from profile
+//@access   Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+        
+        // Get remove index
+        const removeIndex = profile.experience
+            .map(item => item.id)
+            .indexOf(req.params.exp_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send('Server error');
+    }
+
+});
 
 module.exports = router;

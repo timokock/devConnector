7bea096c81521bfd0d6a8d0dbd1b024d85b0d633
Timo Kock <timo.kock@movicoders.com>
2020-01-20 13:43:08 +0100

    Get All Profiles & Profile By User ID
    Added feature Get All Profiles & Profile By User ID

--- models/User.js
+++ models/User.js
@@ -23,4 +23,4 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
-module.exports = User = mongoose.model('User', UserSchema);
+module.exports = User = mongoose.model('user', UserSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -93,5 +93,38 @@ router.post('/', [ auth,
 
 });
 
+//@route    GET api/profile
+//@desc     Get all profiles
+//@access   Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+//@route    GET api/profile/user/:user_id
+//@desc     Get profile by user id
+//@access   Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+        
+        if(!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found' });
+        };
+        res.status(500).send('Server error');
+    }
+});
+
 
 module.exports = router;

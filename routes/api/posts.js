c0584662f7d1d7eb0abdb561c9a3cedcb1a8556b
Timo Kock <timo.kock@movicoders.com>
2020-01-21 09:35:25 +0100

    Post Like & Unlike Routes
    Added feature like and unlike a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -104,4 +104,52 @@ router.delete('/:id', auth, async (req, res) => {
     }
 });
 
+//@route    PUT api/posts/like/:id
+//@desc     Like a post
+//@access   Private
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // First check if the post have already been liked
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ msg: 'Post already liked' });
+        } else {
+            post.likes.unshift({ user: req.user.id });
+            await post.save();
+            return res.json(post.likes);
+        };
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+});
+
+//@route    PUT api/posts/unlike/:id
+//@desc     Unlike a post
+//@access   Private
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // First check if the post have already been liked
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+            return res.status(400).json({ msg: 'Post has not yet been liked' });
+        } else {
+            // Get remove index
+            const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+            post.likes.splice(removeIndex, 1);
+            await post.save();
+            res.json(post.likes);
+        };
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error')
+    }
+});
+
+
 module.exports = router;

b63844c6e119b2ad8705694ac50ac4ee946e46e1
Timo Kock <timo.kock@movicoders.com>
2020-01-22 12:37:18 +0100

    Protected Route For Dashboard
    Implemented protected route for dashboard - PrivateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 // REDUX
 import { Provider } from 'react-redux';
 import store from './store';
@@ -29,6 +31,7 @@ const App = () => {
         <Route exact path='/' component={Landing} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
+        <PrivateRoute exact path='/dashboard' component={Dashboard} />
       </Switch>
     </Router>
   </Provider>
---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Dashboard = props => {
+    return (
+        <section class="container">
+            Dashboard
+        </section>
+    )
+}
+
+Dashboard.propTypes = {
+
+}
+
+export default Dashboard;

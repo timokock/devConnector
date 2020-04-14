f65d789f7d57827bf652621aa7009f78af7d51be
Timo Kock <timo.kock@movicoders.com>
2020-04-14 18:09:59 +0200

    Post Item Component
    Added the PostItem component

--- client/src/components/posts/Post.js
+++ client/src/components/posts/Post.js
@@ -2,18 +2,26 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import PostItem from './PostItem';
 import { getPosts } from '../../actions/post';
 
 
-const Post = ({ getPosts, post: { post, loading } }) => {
+const Post = ({ getPosts, post: { posts, loading } }) => {
     useEffect(() => {
         getPosts();
     }, [getPosts]);
 
-    return (
-        <div>
-            
-        </div>
+    return loading ? <Spinner /> : (
+        <Fragment>
+            <section className="container">
+                <h1 className="large text-primary">Posts</h1>
+                <p className="lead"><i className="fas fa-user"></i> Welcome to the community</p>
+                {/* PostForm */}
+                <div className="posts">{posts.map(post => (
+                    <PostItem key={post._id} post={post} />
+                ))}</div>
+            </section>
+        </Fragment>
     )
 }
 

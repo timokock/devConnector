09443b40385aaeccabd834a95d8eb55193743b0f
Timo Kock <timo.kock@movicoders.com>
2020-04-14 10:40:45 +0200

    Post Reducer, Action & Initial Component
    Implemented post reducer, action and initial component

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import AddExperience from './components/profile-forms/AddExperience';
 import AddEducation from './components/profile-forms/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Post';
 import PrivateRoute from './components/routing/PrivateRoute';
 // REDUX
 import { Provider } from 'react-redux';
@@ -50,6 +51,7 @@ const App = () => {
         <PrivateRoute exact path='/edit-profile' component={EditProfile} />
         <PrivateRoute exact path='/add-experience' component={AddExperience} />
         <PrivateRoute exact path='/add-education' component={AddEducation} />
+        <PrivateRoute exact path='/posts' component={Posts} />
       </Switch>
     </Router>
   </Provider>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -10,6 +10,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     const authLinks = (
         <ul>
             <li><Link to="/profiles">Developers</Link></li>
+            <li><Link to="/posts">Posts</Link></li>
             <li><Link to="/dashboard"><i className="fas fa-user"></i>{' '}<span className="hide-sm">Dashboard</span></Link></li>
             <li><Link to="!#" onClick={logout}><i className="fas fa-sign-out-alt"></i>{' '}<span className="hide-sm">Logout</span></Link></li>
         </ul>

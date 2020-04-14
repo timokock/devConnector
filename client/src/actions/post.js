52893d4f171954eedd22954faae67fa95bbc7e40
Timo Kock <timo.kock@movicoders.com>
2020-04-14 18:57:05 +0200

    Like & Unlike Functionality
    Implemented the like and unlike feature

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR } from './types';
+//import { setAlert } from './alert';
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
 // Get posts
-
 export const getPosts = () => async dispatch => {
     try {
         const res = await axios.get('/api/posts');
@@ -11,6 +10,40 @@ export const getPosts = () => async dispatch => {
         dispatch({
             type: GET_POSTS,
             payload: res.data
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });        
+    }
+};
+
+// Add like
+export const addLike = (postID) => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/like/${postID}`);
+
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { postID, likes: res.data }
+        });
+    } catch (err) {
+        dispatch({
+            type: POST_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        });        
+    }
+};
+
+// Remove like
+export const removeLike = (postID) => async dispatch => {
+    try {
+        const res = await axios.put(`/api/posts/unlike/${postID}`);
+
+        dispatch({
+            type: UPDATE_LIKES,
+            payload: { postID, likes: res.data }
         });
     } catch (err) {
         dispatch({
